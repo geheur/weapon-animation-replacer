bc7c33c4235ce1c6da8e0b0f701a84ac212bc2f4
sam <dasistkeinnamen@gmail.com>
2022-09-11 11:42:03 -0700

    Update plugin test name so it's the same as the rest of my plugins.

--- src/test/java/com/weaponanimationreplacer/ExamplePluginTest.java
+++ src/test/java/com/weaponanimationreplacer/ExamplePluginTest.java
@@ -8,7 +8,7 @@ import net.runelite.client.RuneLite;
 import net.runelite.client.RuneLiteProperties;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class WeaponAnimationReplacerPluginTest
+public class ExamplePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
